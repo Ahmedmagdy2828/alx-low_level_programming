aabcd91e0da3b86e89cff4e59e264702ec324be2
Ahmedmagdy2828 <Ahmedmagdy@example.com>
2023-09-08 16:51:52 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Entry point
+*
+*description:rint_alphabet
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+	return (0);
+}
+
