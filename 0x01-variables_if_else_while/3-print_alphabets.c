2bd3a3d9b61a7a4863d420acaf608d20123fb384
Ahmedmagdy2828 <Ahmedmagdy@example.com>
2023-09-08 16:06:35 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Entry point
+*
+*description:rint_alphabet
+*
+* Return: 0
+*/
+
+int main(void)
+{
+char low;
+
+for (low = 'a' ; low <= 'z' ; low++)
+putchar(low);
+
+for (low = 'A' ; low <= 'z' ; low++)
+putchar(low);
+putchar('\n');
+return (0);
+}
