005819cba93a8b0f5bcb1ff87f6af8e079eb778c
Ahmedmagdy2828 <Ahmedmagdy@example.com>
2023-09-08 15:31:29 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*description:rint_alphabet
+*
+*Return: 0
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
